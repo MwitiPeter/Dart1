4332716740b57c6b7c6fa25558b48e9997f1328c
PeterMwitiDev <mwitipet@gmal.com>
2024-08-19 14:53:53 +0300

    added functions

---
+++ anornymous.dart
@@ -0,0 +1,38 @@
+// void main() {
+//   const fruits = ["apple", "orange", "banana"];
+
+//   fruits.forEach((fruit) {
+//     print(fruit);
+//   });
+// }
+// void main() {
+//   List cars = ["BMW", "Toyota"];
+
+//   cars.forEach((car) {
+//     print(car);
+//   });
+// }
+// double calculateInterest(double principal, double rate, double time) {
+//   double intrest = principal * rate * time;
+//   return intrest;
+// }
+
+// void main() {
+// double principal = 5000;
+// double time = 3;
+// double rate = 3;
+
+// double result = calculateInterest(principal, rate, time);
+// print(result);
+// }
+
+// double calculateInterest(double principal, double time, double rate) =>
+//     principal * rate * time;
+// void main() {
+//   double principal = 5000;
+//   double time = 3;
+//   double rate = 3;
+
+//   double result = calculateInterest(principal, rate, time);
+//   print(result);
+// }
---
+++ finction.dart
@@ -0,0 +1,51 @@
+// void printName() {
+//   print("My name is Peter");
+// }
+
+// void main() {
+//   printName();
+// }
+
+// void addnumbers(int num1, int num2) {
+//   int sum = num1 + num2;
+//   print("sum is $sum");
+// }
+
+// void main() {
+//   addnumbers(10, 12);
+// }
+
+// void printName() {
+//   print("My name is john");
+// }
+
+// void main() {
+//   printName();
+// }
+// void printName(String name) {
+//   print("welcome,$name");
+// }
+
+// void main() {
+//   printName('john');
+// }
+// void main() {
+//   String name = addName();
+//   print("The name fromfunction is $name");
+// }
+
+// String addName() {
+//   return "allan";
+// }
+
+int add(int a, int b) {
+  int sum = a + b;
+  return (sum);
+}
+
+void main() {
+  int num1 = 12;
+  int num2 = 13;
+  int total = add(num1, num2);
+  print("$total");
+}
